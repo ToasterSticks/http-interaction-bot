326fd78e38db7b491bce9bd274a371ad2b9f9b19
ToastOnAStick <matt.zhao.18@gmail.com>
2022-09-18 22:22:25 -0400

    fix: export formdata

--- src/http-interactions/index.ts
+++ src/http-interactions/index.ts
@@ -1,3 +1,4 @@
 export type { Application, Command } from './handler';
 export { createApplicationCommandHandler } from './handler';
+export { formDataResponse } from './interaction';
 export * from './types';
