93612723aaa77ec88143eaa240b0bd41701cff83
ToastOnAStick <matt.zhao.18@gmail.com>
2023-09-16 09:49:19 -0400

    feat: autocomplete support

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,18 @@
+import type {
+	APIApplicationCommandInteractionDataIntegerOption,
+	APIApplicationCommandInteractionDataNumberOption,
+	APIApplicationCommandInteractionDataStringOption,
+} from 'discord-api-types/v10';
+
 declare global {
 	const CLIENT_ID: string;
 	const CLIENT_SECRET: string;
 	const PUBLIC_KEY: string;
 }
+
+export type APIApplicationCommandInteractionDataAutocompleteOption =
+	| APIApplicationCommandInteractionDataStringOption
+	| APIApplicationCommandInteractionDataIntegerOption
+	| APIApplicationCommandInteractionDataNumberOption;
 
 export {};
